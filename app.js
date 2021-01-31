b82bc594661d545229e14439203106d8bc9d877e
Rohald van Merode <rohald_89@live.nl>
2021-01-31 14:56:28 +0100

    added error handlers and created the templates

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 const Sequelize = require('sequelize');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
@@ -23,35 +41,42 @@ const sequelize = new Sequelize({
   }
 })();
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// catch 404 errors
+app.use((req, res, next) => {
+  console.log('404 error handler called');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).render('not-found', {title: "Not Found"});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Global error handler 
+app.use((err, req, res, next) => {
+  if(err){
+    console.log('Global error handler called', err);
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(err.status === 404) {
+    res.status(404).render('error', {err});
+  } else {
+    err.message = err.message || `Oops! It looks like something went wrong on the server.`;
+    res.status(err.status || 500).render('error', {err});
+  }
 });
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  const books = await Book.findAll();
+  console.log(books.map(book => book.toJSON()));
+  res.render('index', { books, title: 'Books' });
+})
+);
 
 module.exports = router;
