68638cb0ceee46f917ad818aad186d93d191cc2e
Rohald van Merode <rohald_89@live.nl>
2021-01-31 17:38:53 +0100

    added post routes to update and delete books

--- routes/books.js
+++ routes/books.js
@@ -33,12 +33,52 @@ router.post('/new', asyncHandler(async (req, res) => {
   } catch (error) {
     if(error.name === "SequelizeValidationError") { // checking the error
       book = await Book.build(req.body);
-      console.log(book);
       res.render("new-book", { book , errors: error.errors, title: "New Book" });
     } else {
       throw error; // error caught in the asyncHandler's catch block
     } 
   }
 }));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book){
+    res.render('update-book', {book, title: "Edit Book" });
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+router.post('/:id', asyncHandler(async (req, res) => {
+  let book; 
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book){
+      await book.update(req.body);
+      res.redirect('/books/');
+    } else {
+      res.sendStatus(404);
+    }
+  } catch(error) {
+    if(error.name === "SequelizeValidationError"){
+      book = await Book.build(req.body);
+      book.id = req.params.id;
+      res.render('update-book', {book, errors: error.errors, title: "Update Book"});
+    } else {
+      throw error;
+    }
+  }
+}));
+
+router.post('/:id/delete', asyncHandler(async (req,res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    await book.destroy();
+    res.redirect('/books');
+  } else {
+    res.sendStatus(404);
+  }
+}))
+
 
 module.exports = router;
